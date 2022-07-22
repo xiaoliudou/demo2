15e85433ffa680fccc9235ba8af72a3f49700759
dwchenyuhao_a <chenyuhao_a@aspirecn.com>
2022-07-22 14:02:22 +0800

    commit

--- demo.java
+++ demo.java
@@ -2,5 +2,9 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("a");
     }
+    BaseDTO method (){
+        BaseDTO baseDTO = new BaseDTO();
+        return baseDTO;
+    }
 
 }
